320236af0fd469f9223e27391290c6be0ead11bb
rajat <rjtkpanda@gmail.com>
2018-02-12 22:17:29 +0100

    added the modifed files

--- data_generator.cc
+++ data_generator.cc
@@ -24,8 +24,11 @@ int main()
 {
   std::fstream myfile;
   srand(time(0));
+  int n;
+  std::cout << "Enter the number of data that you want to generate" << '\n';
+  std::cin >> n;
   myfile.open ("example");
-  for (size_t i = 0; i < 10; i++) {
+  for (int i = 0; i < n; i++) {
     std::string str;
     for(int z=0; z < 5; z++){
     str +=  genRandom();
@@ -35,6 +38,4 @@ int main()
 
   myfile.close();
   return 0;
-
-
 }
